437df856113665c90bc874e3ff8b6f55371b74f0
nadeemgudaganur <51920004+nadeemgudaganur@users.noreply.github.com>
2025-03-01 14:07:36 +0530

    Basic nodejs server

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const PORT = 4000;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
